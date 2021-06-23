20db295ac8c901ca835e08dfb4f346038473ae43
abdalla-atalla <60201830+abdalla-atalla@users.noreply.github.com>
2021-06-22 22:25:09 -0700

    file is changed now

--- src/model/AssistantJack.java
+++ src/model/AssistantJack.java
@@ -27,7 +27,7 @@ public class AssistantJack {
     }
 
     /**
-     * Changes to filed
+     * Changes to filess
      * The full constructor, specifying which assistant to use (answer set 1,
      * answer set 2, or random). Depending on assistant, a different correct
      * Theory object is created.
